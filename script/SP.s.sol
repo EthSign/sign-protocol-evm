bce8880307fe93f79d53bd537e27cbc15347e13c
boyuanx <boyuanxu@usc.edu>
2024-01-01 11:25:50 +0800

    Fix upgrade script

--- script/SP.s.sol
+++ script/SP.s.sol
@@ -2,12 +2,13 @@
 pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
-import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
+import {Upgrades, Options} from "openzeppelin-foundry-upgrades/Upgrades.sol";
 import {SP} from "../src/core/SP.sol";
 
 contract SPDeploymentScript is Script {
     function run() public {
-        _deploy();
+        // _deploy();
+        _upgrade();
     }
 
     function _deploy() internal {
@@ -22,7 +23,19 @@ contract SPDeploymentScript is Script {
     function _upgrade() internal {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
-        Upgrades.upgradeProxy(0xcF220423c825d6fBAcE852eF5edCb0D9e8499AC5, "SP.sol", abi.encodeCall(SP.initialize, ()));
+        Upgrades.upgradeProxy(
+            0x76459e57b9C6D5Ee74FAcf841c36a17FC2fD1a05,
+            "SP.sol",
+            "",
+            Options({
+                referenceContract: "SP-old.sol:SP",
+                constructorData: "",
+                unsafeAllow: "",
+                unsafeAllowRenames: false,
+                unsafeSkipStorageCheck: false,
+                unsafeSkipAllChecks: false
+            })
+        );
         vm.stopBroadcast();
     }
 }
