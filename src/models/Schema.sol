40eb9387114a0d34420f11639bba2e09d5c7f403
boyuanx <boyuanxu@usc.edu>
2024-01-01 11:34:14 +0800

    Fix docs typo

--- src/models/Schema.sol
+++ src/models/Schema.sol
@@ -13,7 +13,7 @@ import {DataLocation} from "./DataLocation.sol";
  * `schemaDataLocation`: Where `Schema.data` is stored. See `DataLocation.DataLocation`.
  * `attestationDataLocation`: Where `Attestation.data` is stored. See `DataLocation.DataLocation`.
  * `maxValidFor`: The maximum number of seconds that an Attestation can remain valid. 0 means Attestations can be valid forever. This is enforced through `Attestation.validUntil`.
- * `resolver`: The `ISPResolver` that is called at the end of every function. 0 means there is no resolver is set. See `ISPResolver`.
+ * `resolver`: The `ISPResolver` that is called at the end of every function. 0 means there is no resolver set. See `ISPResolver`.
  * `data`: The raw schema that `Attestation.data` should follow. Since there is no way to enforce this, it is a `string` for easy readability.
  */
 struct Schema {
