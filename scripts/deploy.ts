912aee17fc16066fd588efdf43ae782a518f5472
boyuanx <boyuanxu@usc.edu>
2024-02-08 02:35:07 -0800

    Deployment updates

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,7 +2,7 @@ import { ethers, upgrades } from "hardhat";
 
 async function main() {
   const SP = await ethers.getContractFactory("SP");
-  const sp = await upgrades.deployProxy(SP, []);
+  const sp = await upgrades.deployProxy(SP, [1, 1]);
   await sp.waitForDeployment();
   console.log("SP deployed to:", await sp.getAddress());
 }
