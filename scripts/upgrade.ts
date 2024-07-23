cfb75f3f751b1186b26f95e061996379041c8d08
boyuanx <jxu@ethsign.xyz>
2024-07-23 15:03:32 -0700

    Upgrade Arbitrum deployments

--- scripts/upgrade.ts
+++ scripts/upgrade.ts
@@ -2,7 +2,7 @@ import { ethers, upgrades } from "hardhat";
 
 async function main() {
   const SP = await ethers.getContractFactory("SP");
-  const sp = await upgrades.upgradeProxy("0xEadFcE1eA8c2BB0DE3Cc3854076E1900373Aae59", SP);
+  const sp = await upgrades.upgradeProxy("0x4e4af2a21ebf62850fD99Eb6253E1eFBb56098cD", SP);
   await sp.waitForDeployment();
 }
 
